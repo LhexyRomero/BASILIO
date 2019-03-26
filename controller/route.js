22011f4f77bf020c28aa66899459e3c72bf9fdca
lhexyromero <lhexyromero@gmail.com>
2019-03-26 15:56:03 +0800

    Admin Partials

--- controller/route.js
+++ controller/route.js
@@ -4,4 +4,24 @@ var router = express.Router();
 router.use('/admin',require('./direction/admin'));/* 
 router.use('/basilio',require('./direction/user')); */
 
+router.use((req,res)=>{
+    let response;
+    if(req.xhr){
+        response = {success: false};
+    }else{
+        response = "<center><h1>404 Page Not Found</h1><br/><p>The page you been looking for must be remove</p></center>";
+    }
+    res.status(404).send(response);
+});
+
+router.use((err,req,res,next)=>{
+    console.error(err.message, err.stack);
+    let response;
+    if(req.xhr){
+        response = {success: false};
+    }else{
+        response = "<center><h1>Error: 500</h1><br/><p>Internal Server Error. We'll fix it soon. ;)</p></center>";
+    }
+    if(!res.headerSent) res.status(500).send(response);
+});
 module.exports = router;
